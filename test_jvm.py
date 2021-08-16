de1114ae0cfac74f3915f3c21c22c98dd7478b80
Leandro Rometsch <lr@kuppingercole.com>
2021-08-16 14:12:34 +0200

    added meaningful jvm test

--- test_jvm.py
+++ test_jvm.py
@@ -7,8 +7,19 @@ class TestJvm(unittest.TestCase):
     hw = HotWalletWrapper()
 
     def test_keygen(self):
-        skey = self.cw.master_gen().getKeySec()
-        self.assertTrue(skey)
+        test_key = self.cw.master_gen()
+
+        sec_key = test_key.getKeySec()
+        key_type = str(type(sec_key))
+        key_length = len(str(sec_key.toString()))
+        self.assertTrue(key_type == '<java class \'java.math.BigInteger\'>')
+        self.assertTrue(key_length > 100)
+
+        pub_key = test_key.getKeyPub()
+        key_type = str(type(pub_key))
+        key_length = len(str(pub_key.toString()))
+        self.assertTrue(key_type == '<java class \'com.ewallet.field.util.EllipticCurvePoint\'>')
+        self.assertTrue(key_length > 100)
 
 
 if __name__ == '__main__':
