5f5eea926e46e4bab35f08f51432b1a82dd988b5
Leandro Rometsch <lr@kuppingercole.com>
2021-10-12 13:25:19 +0200

    util documentation

--- utils.py
+++ utils.py
@@ -9,6 +9,8 @@ from wrapper import create_elliptic_curve_point
 
 @dataclass
 class PublicKey:
+    """This dataclass wraps all information regarding a derived public key.
+    It includes its ethereum address, its id and the public key coordinates. """
     address: str
     id: int
     x: str
@@ -17,23 +19,43 @@ class PublicKey:
 
 @dataclass
 class PrivateKey:
+    """This dataclass wraps all information regarding a derived private key.
+        It includes the private key and the id used for deriving."""
     key: str
     id: int
 
 
-def save_dict_to_file(path, state_dict: dict):
+def save_dict_to_file(path, data: dict):
+    """
+    Allows to store any dictionary in a file under the given path.
+
+    :param path: the path where the dictionary should be stored at
+    :param dict data: the state to be stored
+    """
     with open(path, 'w') as txt_file:
-        json.dump(state_dict, txt_file)
+        json.dump(data, txt_file)
         txt_file.close()
 
 
 def get_dict_from_file(path) -> dict:
+    """
+    Allows to load any dictionary (that has been stored via save_dict_to_file()) from a file under the given path.
+
+    :param path: the path where the file is located
+    :return: the loaded dictionary
+    """
     with open(path, 'r') as txt_file:
         data = txt_file.readlines()
         return json.loads(data[0])
 
 
 def get_private_key_from_file(path):
+    """
+    Allows to load a private key from a file under the given path.
+
+    :param path: the path where the private key is located
+    :return: the private key as java BigInteger
+    """
     with open(path, 'r') as key_file:
         data = key_file.readlines()[0]
         key_file.close()
@@ -43,6 +65,12 @@ def get_private_key_from_file(path):
 
 
 def get_public_key_from_file(path):
+    """
+    Allows to load a public key from a file under the given path.
+
+    :param path: the path where the public key is located
+    :return: the private key as a java EllipticCurvePoint
+    """
     with open(path, 'r') as key_file:
         data = key_file.readlines()
         key_file.close()
@@ -51,14 +79,16 @@ def get_public_key_from_file(path):
     return data
 
 
-def write_signature_to_file(path, signature):
-    with open(path, "w") as sig_file:
-        json.dump(signature, sig_file)
-        sig_file.close()
-        return True
-
-
 def delete_files_in_folder(path):
+    """
+    Deletes everything inside a certain directory given as a path.
+    Intended to be used with the overwrite functionality of the wallet.
+    Note that also all sub directories are being deleted.
+    The result is an empty directory under path.
+    Use with caution!
+
+    :param path: the path to the directory which files/directories are to be deleted
+    """
     for root, dirs, files in os.walk(path):
         for file in files:
             os.remove(os.path.join(root, file))
